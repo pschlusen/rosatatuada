6871f504c36d9f8b8b0d823f85a9a6ff41aaa14c
Pablo Schlusen <pablo@trinto.com.br>
2022-02-02 09:04:42 -0300

    feature: add js minify to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
-var gulp = require('gulp')
-var cleanCSS  = require('gulp-clean-css')
-var livereload = require('gulp-livereload')
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const cleanCSS  = require('gulp-clean-css')
+const livereload = require('gulp-livereload')
 const autoprefixer = require('gulp-autoprefixer')
-var browserSync = require('browser-sync').create()
+const browserSync = require('browser-sync').create()
+
+
  
 const paths = { 
     common: {
-        dest: "./dest/css/"
+        dest: "./dest/"
     },
     styles: {
-        src: "src/**/*.css",
-        dest: "dest/css"
+        src: "src/**/*.css"
     },
     scripts: {
-        src: "src/**/*.js",
-        dest: "dest/js"
+        src: "src/**/*.js"
     }
 }
 
 /* serve local files */
 initBrowserSync = () => {
     browserSync.init({
+        open: false,
         server: {
             baseDir: paths.common.dest
         }
@@ -29,21 +32,27 @@ initBrowserSync = () => {
 
 /* reload page when any file is changed */
 initLiveReload = () => {
-    livereload.listen({ basePath: './dest' })
+    livereload.listen({ basePath: paths.common.dest })
 }
 
 minifyCSS = () => {
     return gulp.src(paths.styles.src)
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(gulp.dest(paths.common.dest))
 }
 
+minifyJS = () => {
+    return gulp.src(paths.scripts.src)
+            .pipe(babel({presets: ['@babel/preset-env']}))
+            .pipe(uglify())
+            .pipe(gulp.dest(paths.common.dest))
+}
  
 gulp.task('default', () => {
     initBrowserSync()
     initLiveReload()
-    gulp.watch(paths.styles.src, minifyCSS).on('change', () => {
-        livereload.reload()
-    })
-});
+
+    gulp.watch(paths.scripts.src, minifyJS).on('change', () => { livereload.reload() })
+    gulp.watch(paths.styles.src, minifyCSS).on('change', () => { livereload.reload() })
+})
